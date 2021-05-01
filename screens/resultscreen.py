573b8870a3d086b5440aa6210d19fe87c0c48718
ivana2432 <78886557+ivana2432@users.noreply.github.com>
2021-05-01 16:55:01 +0700

    Update resultscreen.py

--- screens/resultscreen.py
+++ screens/resultscreen.py
@@ -1,29 +1,29 @@
 from kivy.uix.screenmanager import Screen
 from kivy.properties import ObjectProperty
-from kivymd.uix.dialog import MDDialog
-from verifypinscreen import VerifyDiary
 from database import Database
 
 db = Database()
 
-#screen ganti pin
-class GantiPINScreen(Screen):
+#screen hasil pencarian diary
+class ResultScreen(Screen):
 
-    pin = ObjectProperty(None)
+    diarytext = ObjectProperty(None)
+    title = ObjectProperty(None)
 
-    def ganti(self):
-        #jika pin yang diisi sesuai
-        if VerifyDiary().changepin() == self.pin.text:
-            self.manager.get_screen('buatpin').fill()
-            self.pin.text = ''
-            return 'buatpin' #pindah ke screen buat pin
-        #jika pin yang diisi tidak sesuai
+    def searchtext(self): #menampilkan diary yang telah dibuat sesuai tanggal yang diinput
+        self.title.text = db.searchtitle(self.manager.get_screen('history').balik())
+        if len(db.searchdiary(self.manager.get_screen('history').balik())) >= 200 :
+            #jika teksnya melebihi 200 karakter, tampilkan sebagian saja
+            self.diarytext.text = db.searchdiary(self.manager.get_screen('history').balik())[:200]+'...'
         else:
-            invalidpin = MDDialog(text='PIN salah',
-                                  size_hint=(0.6, 0.2))
-            invalidpin.open()
-            self.pin.text = ''
-            return 'gantipin' #tetap di screen ganti pin
+            self.diarytext.text = db.searchdiary(self.manager.get_screen('history').balik())
+
+    def edit(self): #pindah ke diary screen dengan mode edit
+        self.manager.get_screen('diary').showdata(db.searchdiary(self.manager.get_screen('history').balik()),
+                                                  self.manager.get_screen('history').balik(),
+                                                  self.title.text)
 
     def clear(self):
-        self.pin.text = ''
+        #membersihkan text pencarian
+        self.manager.get_screen('history').clear()
+
