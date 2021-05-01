63e4dc1dae43b6696c01eadb1110e0914a28e63d
ivana2432 <78886557+ivana2432@users.noreply.github.com>
2021-05-01 16:54:01 +0700

    Update lupapinscreen.py

--- screens/lupapinscreen.py
+++ screens/lupapinscreen.py
@@ -5,20 +5,25 @@ from database import Database
 
 db = Database()
 
+#scren untuk lupa pin
 class LupaPin(Screen):
+
     jawaban = ObjectProperty(None)
     pertanyaan = ObjectProperty(None)
 
     def showpertanyaan(self):
+        #menampilkan pertanyaan yang sesuai saat membuat pin
         self.pertanyaan.text = db.question()
 
     def lupapin(self):
         if self.jawaban.text == db.forget():
             self.manager.get_screen('buatpin').fill()
-            return 'buatpin'
+            return 'buatpin' #jika jawaban sesuai, arahkan untuk membuat pin ulang
         else:
             salah = MDDialog(text='Jawaban Anda salah.',
                              size_hint=(0.7,0.2))
+            salah.open() #tampilkan popup jika jawaban tidak sesuai
+            return 'lupapin'
 
     def clear(self):
         self.jawaban.text = ''
